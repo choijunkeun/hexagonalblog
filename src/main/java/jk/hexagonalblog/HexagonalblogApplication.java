5bfa83dbc995d5b7e2ad83a9a7409476cdf800a1
jan <bonawillis@naver.com>
2023-12-05 01:35:04 +0900

    init: Hexagonal Architecture,JPA Blog Project Start

---
+++ src/main/java/jk/hexagonalblog/HexagonalblogApplication.java
@@ -0,0 +1,13 @@
+package jk.hexagonalblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jk/hexagonalblog/HexagonalblogApplicationTests.java
@@ -0,0 +1,13 @@
+package jk.hexagonalblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
